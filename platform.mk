6efca542445849aafa876078154ee97ba0a343f1
mdeejay <mdjrussia@gmail.com>
2019-08-03 03:38:50 +0200

    sdm660-common: Switch to MiuiCamera
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- platform.mk
+++ platform.mk
@@ -109,8 +109,7 @@ PRODUCT_PACKAGES += \
 	camera.device@3.2-impl \
 	libxml2 \
 	vendor.qti.hardware.camera.device@1.0 \
-	vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+	vendor.qti.hardware.camera.device@1.0_vendor
 
 # CNE
 PRODUCT_PACKAGES += \
